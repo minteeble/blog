88a9e189bbbd306a61b9b8c38bbc28d2b3098372
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-09-17 18:30:02 +0200

    share post

--- src/components/ArticleBody/ArticleBody.tsx
+++ src/components/ArticleBody/ArticleBody.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { ArticleBodyProps } from "./ArticleBody.types";
-import { FacebookShareButton } from "react-share";
-import { FacebookIcon } from "react-share";
+import {
+  FacebookShareButton,
+  WhatsappShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  EmailShareButton,
+} from "react-share";
+import {
+  FacebookIcon,
+  WhatsappIcon,
+  TwitterIcon,
+  TelegramIcon,
+  EmailIcon,
+} from "react-share";
 
 const ArticleBody = (props: ArticleBodyProps) => {
   return (
@@ -18,10 +30,65 @@ const ArticleBody = (props: ArticleBodyProps) => {
           dangerouslySetInnerHTML={{ __html: props.content }}
         ></p>
         <div className="article-body-share">
-          <h4 className="spaced">share this post</h4>
-          <FacebookShareButton url={window.location.href}>
-            <FacebookIcon></FacebookIcon>
-          </FacebookShareButton>
+          <h4 className="article-body-share-title spaced">share this post</h4>
+          <div className="article-body-share-social">
+            <WhatsappShareButton
+              className="article-body-share-social-item"
+              url={window.location.href}
+            >
+              <WhatsappIcon
+                round={true}
+                bgStyle={{ display: "none" }}
+                iconFillColor={"#25D366"}
+                size={45}
+              ></WhatsappIcon>
+            </WhatsappShareButton>
+            <FacebookShareButton
+              className="article-body-share-social-item"
+              url={window.location.href}
+              quote={"Read this article"}
+            >
+              <FacebookIcon
+                round={true}
+                bgStyle={{ display: "none" }}
+                iconFillColor={"#3b5998"}
+                size={60}
+              ></FacebookIcon>
+            </FacebookShareButton>
+            <TelegramShareButton
+              className="article-body-share-social-item"
+              url={window.location.href}
+            >
+              <TelegramIcon
+                round={true}
+                bgStyle={{ display: "none" }}
+                iconFillColor={"#229ED9"}
+                size={45}
+              ></TelegramIcon>
+            </TelegramShareButton>
+            <TwitterShareButton
+              className="article-body-share-social-item"
+              url={window.location.href}
+            >
+              <TwitterIcon
+                round={true}
+                bgStyle={{ display: "none" }}
+                iconFillColor={"#1DA1F2"}
+                size={60}
+              ></TwitterIcon>
+            </TwitterShareButton>
+            <EmailShareButton
+              className="article-body-share-social-item"
+              url={window.location.href}
+            >
+              <EmailIcon
+                round={true}
+                bgStyle={{ display: "none" }}
+                iconFillColor={"#ff4c4c"}
+                size={60}
+              ></EmailIcon>
+            </EmailShareButton>
+          </div>
           <span className="article-body-share-line"></span>
         </div>
       </div>
