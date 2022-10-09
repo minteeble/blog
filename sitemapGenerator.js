7b44da872e182487f5e96f3f28cf456248c905b0
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-10-09 13:46:37 +0200

    sitemap

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -1,16 +1,55 @@
 const format = require("xml-formatter");
 const fs = require("fs");
+const axios = require("axios");
 
-let postUrls = [
-  {
-    loc: "URL1",
-    lastmod: "TIMESTAMP",
+const endpoint = "https://cms-blog-backend.minteeble.com/mintql";
+
+const query = `{
+  posts {
+    edges {
+      node {
+        language {
+          slug
+        }
+        categories {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+        slug
+        modifiedGmt
+      }
+    }
+  }
+}`;
+
+axios({
+  url: endpoint,
+  method: "post",
+  data: {
+    query: query,
   },
-];
+}).then((result) => {
+  let urls = [];
+  let short = result.data.data.posts.edges;
+  const node =
+    result.data.data.posts.edges.length > 0
+      ? result.data.data.posts.edges.length
+      : 0;
+  for (let i = 0; i < node; i++) {
+    let x = {
+      loc: `https://blog-test.minteeble.com/${short[i].node.language.slug}/${short[i].node.categories.edges[0].node.slug}/${short[i].node.slug}`,
+      lastmod: short[i].node.modifiedGmt,
+    };
+    urls.push(x);
+  }
 
-setTimeout(() => {
-  fs.writeFileSync("./public/sitemap.xml", generateSitemapItems(postUrls));
-}, 500);
+  console.log(node);
+
+  fs.writeFileSync("./public/sitemap.xml", generateSitemapItems(urls));
+});
 
 function generateSitemapItems(urls) {
   return format(
