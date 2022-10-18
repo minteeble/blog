a10bedb1877f97b8083b4ac5c5a438b4dc2ceff9
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-10-18 11:01:09 +0200

    some fixes

--- src/browser/index.tsx
+++ src/browser/index.tsx
@@ -2,7 +2,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-// import "./index.css";
 import "../styles/main.scss";
 
 /**
--- src/server/ServerUtils.ts
+++ src/server/ServerUtils.ts
@@ -1,10 +1,29 @@
+//@ts-ignore
 import { MetaInfo } from "./types";
+const axios = require("axios");
 
 export class ServerUtils {
   public static readonly pathRegex = /^[\/](en|it)[\/][A-Za-z0-9_-]+[\/][A-Za-z0-9_-]+/;
 
   public static isPathValid(path: string): boolean {
     return this.pathRegex.test(path);
+  }
+
+  public static getMetaInfo(path: string): MetaInfo {
+    const endpoint = "https://cms-blog-backend.minteeble.com/mintql";
+
+    const query = ``;
+    axios({
+      url: endpoint,
+      method: "post",
+      data: {
+        query: query,
+      },
+    }).then((result) => {
+      if (result.data.data.post === null) {
+      } else {
+      }
+    });
   }
 
   // public static getPostMetaInfo = async (path: string): Promise<MetaInfo> =>  {
